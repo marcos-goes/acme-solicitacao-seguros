b7d82881670d2911e56a7b5f4cdc064b12377741
Marcos Goes <mpsgoes@gmail.com>
2025-07-29 22:04:51 -0300

    TestContainers funcionando...

--- src/test/java/org/mgoes/acme/orders/InsuranceOrderServiceApplicationTests.java
+++ src/test/java/org/mgoes/acme/orders/InsuranceOrderServiceApplicationTests.java
@@ -1,13 +1,13 @@
-//package org.mgoes.acme.orders;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class InsuranceOrderServiceApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package org.mgoes.acme.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceOrderServiceApplicationTests extends TestContainersBase {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/org/mgoes/acme/orders/TestContainersBase.java
@@ -0,0 +1,22 @@
+package org.mgoes.acme.orders;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.testcontainers.containers.ComposeContainer;
+
+import java.io.File;
+
+class TestContainersBase {
+
+    public static ComposeContainer environment = new ComposeContainer(
+            new File("src/test/resources/docker-compose-test/docker-compose.yaml")
+    )
+            .withLocalCompose(true)
+            .withExposedService("postgresql", 5432)
+            .withExposedService("mockserver", 1080)
+            .withExposedService("rabbitmq", 5672);
+
+    @BeforeAll
+    static void startContainers(){
+        environment.start();
+    }
+}
